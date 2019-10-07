590c249809aa0912a4423ea413299a5b40ebde9e
snyder <snyderjosephd@yahoo.com>
2019-10-06 20:12:52 -0500

    Completed Planets and Probes exercise

--- Program.cs
+++ Program.cs
@@ -21,42 +21,35 @@ namespace PlanetChallenge
 
             // Create a list containing KeyValuePairs. Each KeyValuePair will hold the name of a spacecraft that we have launched, and the name of a planet that it has visited. The key of the KeyValuePair will be the probe name, and the value will be the planet it visited.
 
-            List<KeyValuePair<string, string>> probeDestinations = new List<KeyValuePair<string, string>>();
-
-            // Add some planet/probe combinations to the list
-            probeDestinations.Add(new KeyValuePair<string, string>("Viking 1", "Mars"));
-            probeDestinations.Add(new KeyValuePair<string, string>("Mariner 1", "Venus"));
-            probeDestinations.Add(new KeyValuePair<string, string>("Voyager 1", "Jupiter"));
-            probeDestinations.Add(new KeyValuePair<string, string>("Voyager 1", "Saturn"));
+            List<KeyValuePair<string, string>> probeDestinations = new List<KeyValuePair<string, string>>(){
+                new KeyValuePair<string, string>("Viking 1", "Mars"),
+                new KeyValuePair<string, string>("Mariner 1", "Venus"),
+                new KeyValuePair<string, string>("Voyager 1", "Jupiter"),
+                new KeyValuePair<string, string>("Voyager 1", "Saturn"),
+                new KeyValuePair<string, string>("Viking 2", "Mars"),
+                new KeyValuePair<string, string>("Viking 2", "Neptune"),
+                new KeyValuePair<string, string>("Viking 2", "Venus")
+            };
 
             // Iterate over planetList, and inside that loop, iterate over the list of key-value pairs. Write to the console, for each planet, which probes have visited it.
 
             foreach (string planet in planetList)
             {
-                Console.WriteLine(planet);
+                // List to store probes that visited the planet
+                List<string> matchingProbes = new List<string>();
+
+                // Iterate probe destinations and add the probe to the planet's matching probes if the value matches the current planet
+                foreach(KeyValuePair<string, string> kvp in probeDestinations)
+                {
+                    if (kvp.Value == planet)
+                    {
+                        matchingProbes.Add(kvp.Key);
+                    }
+                }
+
+                // Print the planet and the joined list of probes that have been to that planet
+                Console.WriteLine($"{planet}: {String.Join(", ", matchingProbes)}");
             }
-
-            // // Iterate planets
-            // foreach (string planet in planetList)
-            // {
-            //     // List to store probes that visited the planet
-            //     List<string> matchingProbes = new List<string>();
-
-            //     // Iterate probeDestinations
-            //     foreach ()
-            //     {
-            //             Does the current probe's destination
-            //             match the value of the `planet` variable?
-            //             If so, add it to the list.
-            //     }
-
-            //         Use String.Join(",", matchingProbes) as part of the
-            //         solution to get the output below. It's the C# way of
-            //         writing `array.join(",")` in JavaScript.
-            //     Console.WriteLine($"{}: {}");
-            // }
-
-
 
         }
     }
